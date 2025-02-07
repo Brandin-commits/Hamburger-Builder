140e10f15f4550f1c94095aa729c57e0e845a778
Brandin-commits <brandin00mitchell@gmail.com>
2025-02-07 03:12:09 +0000

    Going_to_add_more_later

--- Workspace.py
+++ Workspace.py
@@ -1,15 +1,45 @@
 import random
-#Make hamburger with random ingredients each time
-protein = ['beef']
+#Make food items on demand
 
-cheese = ['american', 'provalone', 'chedder', 'swiss', 'pepper jack']
+drinks = ['water', 'coca-cola', 'sprite', 'lemonade', 'orange juice', ]
+
+protein = ['beef', 'grilled chicken', 'crispy chicken']
+
+main_salad = ['iceberg lettuce', 'kale']
+
+cheese = ['american', 'provalone', 'chedder', 'swiss', 'pepper jack', 'shredded', 'no']
 
 bun = ['sesame', 'whole wheat', 'lettuce', 'plain']
 
 condiment = ['ketchup', 'mayo', 'mustard']
 
-topping = ['onion', 'lettuce', 'tomato', 'mushroom']
+topping = ['onion', 'mushroom']
+
+veggies = ['lettuce', 'tomato', 'spinach']
+
+dressings = ['italian', 'ranch', 'no', ]
+
+def burger():
+    if user_food == 'burger':
+        print('Here\'s a burger with ' + burger_ingre + ' in it.')
+
+def salad():
+    if user_food == 'salad':
+        print('Here\'s a salad with ' + salad_ingre + ' in it.')
+
+burger_ingre = random.choice(bun), random.choice(protein), random.choice(cheese), random.choice(topping), random.choice(condiment)
+
+salad_ingre = random.choice(main_salad), random.choice(cheese), random.choice(veggies), random.choice(dressings)
+
+
+
+user_food = input('What do you want to make?: ').lower().strip()
+
+
+
+
+
 
 #Print one of each
-print('Here\'s a hamburger with a '+  random.choice(protein),'patty, '+ random.choice(cheese),'cheese, '+ random.choice(bun),'bun, '+ random.choice(condiment),'and, '+ random.choice(topping)+'!')
+print('Here\'s a '+ user_food + ' with a '+  random.choice(protein),', '+ random.choice(cheese),'cheese, '+ random.choice(bun),'bun, '+ random.choice(condiment),'and, '+ random.choice(topping)+'!')
 #ChatGPT used to find out how to use 'random.choice' instead of 'random.protein'
