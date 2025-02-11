6898ae95cbd9bdf2f2e470af8bf2ba44bee6595b
Brandin-commits <brandin00mitchell@gmail.com>
2025-02-11 20:28:29 +0000

    hi

--- Workspace.py
+++ Workspace.py
@@ -1,53 +1,46 @@
 import random
 #Make a orderable menu
 
-drinks = ['water', 'coca-cola', 'sprite', 'lemonade', 'orange juice']
-
-sides = ['fries', 'mashed potatoes', 'veggies', 'salad', 'soup']
-
-dessert = ['brownie', 'cheese cake', 'carrot cake']
-
-food = ['cheese burger', 'grilled cheese', 'BLT', 'fried chicken', 'grilled chicken sandwich']
 
 
 def user_order():
-    print(user_food, user_side, user_drink, user_dessert)
+    
+    
+    print(f'Here\'s your order: {order})
 
 
 def user_drink():
-    print('Here are our drink options: ')
+    drinks = ['water', 'coca-cola', 'sprite', 'lemonade', 'orange juice']
+    print(f'Here are our drink options: {drinks}')
 
 
 def user_side():
-    print('How about a side? \nThere\'s:' + sides + '\n')
+    sides = ['fries', 'mashed potatoes', 'veggies', 'salad', 'soup']
+    print(f'How about a side? \nThere\'s:{sides} \n')
 
 
 def user_dessert():
-    print('What would you like for dessert?\nOur menu consists of: ' + dessert + '\nWhat would you like?: ')
+    desserts = ['brownie', 'cheese cake', 'carrot cake']
+    print(f'What would you like for dessert?\nOur menu consists of: {desserts} \nWhat would you like?: ')
 
 
 def user_food():
-    print('What entree would you like?\nWe have: ' + food + '\nWhat would you like?: ')
-
+    food = ['cheese burger', 'grilled cheese', 'BLT', 'fried chicken', 'grilled chicken sandwich']
+    print(f'What entree would you like?\nWe have:  {food} \nWhat would you like?: ')
+    
 
 def burger():
     if user_food == 'burger':
-        print(f'Here\'s a burger with ' + burger_ingre + ' in it.')
+        print(f'Here\'s a burger with {burger} in it.')
 
 
 def salad():
     if user_food == 'salad':
-        print(f'Here\'s a salad with ' + salad_ingre + ' in it.')
+        print(f'Here\'s a salad with {salad} in it.')
 
 
 def main():
-    
-    if user_food == 'burger':
-        burger()
-    elif user_food == 'salad':
-        salad()
-    else:
-        print('Option not available. ')
+    user_food()
 
 
 main()
