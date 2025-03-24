e017731aac334eafdb77dd0b4e5ccd68798aeb71
Brandin-commits <brandin00mitchell@gmail.com>
2025-03-24 01:57:39 +0000

    done

--- Workspace.py
+++ Workspace.py
@@ -4,12 +4,13 @@ import random
 
 def user_drink():
     drinks = ['water', 'coca-cola', 'sprite', 'lemonade', 'orange juice']
-    drink_choice = input(f'And to drink?\nHere are our options: {drinks}\n')
+    drink_choice = input(f'What would you like to drink?\nHere are our options: {drinks}\n')
     if drink_choice in drinks:
         print(f'Good choice on the {drink_choice}.')
-        return(drink_choice)
+        return drink_choice
     else:
         print('That is not on the menu.')
+        return user_drink()
     
 
 def user_side():
@@ -17,9 +18,10 @@ def user_side():
     side_choice = input(f'How about a side? \nThere\'s: {sides}\n')
     if side_choice in sides:
         print(f'Good choice on the {side_choice}.')
-        return(side_choice)
+        return side_choice
     else:
-        print('That is not on the menu.')   
+        print('That is not on the menu.')
+        return user_side()
 
 
 def user_dessert():
@@ -27,9 +29,10 @@ def user_dessert():
     dessert_choice = input(f'What would you like for dessert?\nOur menu consists of: {desserts} \nWhat would you like?: ')
     if dessert_choice in desserts:
         print(f'Good choice on the {dessert_choice}.')
-        return(dessert_choice)
+        return dessert_choice
     else:
         print('That is not on the menu.')
+        return user_dessert()
 
 
 def user_food():
@@ -37,21 +40,28 @@ def user_food():
     food_choice = input(f'What entree would you like?\nWe have: {food} \nWhat would you like?: ').strip().lower()
     if food_choice in food:
         print(f'Great you\'ll be having, {food_choice}.')
-        return(food_choice)
+        return food_choice
     else:
         print('Sorry we don\'t have that.')
+        return user_food()
 
 
-def user_order():
-    print(f'So you be having a drink_choice with food_choice and side_choice. After you\'ll have a dessert_choice.')    
-# I can't get 'item'_choice to be grasped within this function
+def user_order(drink, food, side, dessert):
+    print(f"\nYour final order:")
+    print(f"Drink: {drink}")
+    print(f"Entree: {food}")
+    print(f"Side: {side}")
+    print(f"Dessert: {dessert}")
+    print("Thank you for ordering with us!")
+   
+   # I can't get 'item'_choice to be grasped within this function
 
 def main():
     print('Hi welcome to the restaurant!')
-    user_drink()
-    user_food()
-    user_side()
-    user_dessert()
-    user_order()
+    drink = user_drink()
+    food = user_food()
+    side = user_side()
+    dessert = user_dessert()
+    user_order(drink, food, side, dessert)
 
-user_drink()
+main()
